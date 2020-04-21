5924f053da8472632a1d9b59516bb7de4c285f7e
Shrikanth Ravi <shrikanthravi.me@gmail.com>
2020-04-21 12:48:36 +0530

    Back button bug fix in cart activity

--- app/src/main/java/com/food/ordering/zinger/ui/cart/CartActivity.kt
+++ app/src/main/java/com/food/ordering/zinger/ui/cart/CartActivity.kt
@@ -5,6 +5,7 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -383,5 +384,17 @@ class CartActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
