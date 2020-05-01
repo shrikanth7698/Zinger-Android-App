1a920731c975b74139f7ea33ec1ce31948f012f9
Shrikanth Ravi <shrikanthravi.me@gmail.com>
2020-05-01 14:34:13 +0530

    Order timeline refactored

--- app/src/main/java/com/food/ordering/zinger/ui/order/OrderTimelineAdapter.kt
+++ app/src/main/java/com/food/ordering/zinger/ui/order/OrderTimelineAdapter.kt
@@ -13,6 +13,8 @@ import com.food.ordering.zinger.data.model.OrderItems
 import com.food.ordering.zinger.databinding.ItemOrderStatusBinding
 import com.food.ordering.zinger.utils.AppConstants
 import com.food.ordering.zinger.utils.StatusHelper
+import java.lang.Exception
+import java.text.SimpleDateFormat
 
 class OrderTimelineAdapter(private val context: Context, private val orderStatusList: List<OrderStatus>, private val listener: OnItemClickListener) : RecyclerView.Adapter<OrderTimelineAdapter.OrderTimelineViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup,
@@ -41,12 +43,28 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
                 binding.viewLineTop.visibility = View.VISIBLE
                 binding.viewLineBottom.visibility = View.VISIBLE
             }
+            val orderStatusModel = orderStatus.orderStatusList.firstOrNull {
+                StatusHelper.getStatusMessage(it.orderStatus)==orderStatus.name
+            }
+            if(orderStatusModel!=null){
+                try {
+                    val apiDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+                    val appDateFormat = SimpleDateFormat("hh:mm aaa")
+                    val date = apiDateFormat.parse(orderStatusModel.updatedTime)
+                    val dateString = appDateFormat.format(date)
+                    binding.textTime.text = dateString
+                    //binding.textTimeNew.text = dateString
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
             if (orderStatus.isCurrent) {
                 when (orderStatus.name) {
                     StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_CANCELLED_BY_SELLER),
                     StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_CANCELLED_BY_USER) -> {
+                        binding.textTime.visibility = View.VISIBLE
+                        //binding.textTimeNew.visibility = View.VISIBLE
                         binding.textStatusDesc.visibility = View.VISIBLE
-                        binding.imageStatus.visibility = View.GONE
                         binding.viewCircle.setBackgroundResource(R.drawable.ic_cancel_status)
                         val width = binding.viewCircle.resources.getDimension(R.dimen.status_dot_normal_size)
                         val params = binding.viewCircle.layoutParams
@@ -55,11 +73,13 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
                         binding.viewCircle.layoutParams = params
                         binding.textStatus.setTextSize(TypedValue.COMPLEX_UNIT_PX, binding.textStatus.context.resources.getDimension(R.dimen.status_normal_size))
                         binding.textStatus.setTextColor(ContextCompat.getColor(binding.textStatus.context, android.R.color.black))
+                        binding.textTime.setTextColor(ContextCompat.getColor(binding.textStatus.context, android.R.color.tab_indicator_text))
                     }
                     StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_COMPLETED),
                     StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_DELIVERED),
                     StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_REFUND_COMPLETED) -> {
-                        binding.imageStatus.visibility = View.VISIBLE
+                        binding.textTime.visibility = View.VISIBLE
+                        //binding.textTimeNew.visibility = View.VISIBLE
                         binding.textStatusDesc.visibility = View.VISIBLE
                         binding.viewCircle.setBackgroundResource(R.drawable.ic_checked)
                         val width = binding.viewCircle.resources.getDimension(R.dimen.status_dot_normal_size)
@@ -69,9 +89,11 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
                         binding.viewCircle.layoutParams = params
                         binding.textStatus.setTextSize(TypedValue.COMPLEX_UNIT_PX, binding.textStatus.context.resources.getDimension(R.dimen.status_normal_size))
                         binding.textStatus.setTextColor(ContextCompat.getColor(binding.textStatus.context,android.R.color.black))
+                        binding.textTime.setTextColor(ContextCompat.getColor(binding.textStatus.context,android.R.color.tab_indicator_text))
                     }
                     else -> {
-                        binding.imageStatus.visibility = View.VISIBLE
+                        binding.textTime.visibility = View.VISIBLE
+                        //binding.textTimeNew.visibility = View.VISIBLE
                         binding.textStatusDesc.visibility = View.VISIBLE
                         binding.viewCircle.setBackgroundResource(R.drawable.ic_checked)
                         val width = binding.viewCircle.resources.getDimension(R.dimen.status_dot_normal_size)
@@ -81,6 +103,7 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
                         binding.viewCircle.layoutParams = params
                         binding.textStatus.setTextSize(TypedValue.COMPLEX_UNIT_PX, binding.textStatus.context.resources.getDimension(R.dimen.status_normal_size))
                         binding.textStatus.setTextColor(ContextCompat.getColor(binding.textStatus.context, android.R.color.black))
+                        binding.textTime.setTextColor(ContextCompat.getColor(binding.textStatus.context, android.R.color.tab_indicator_text))
                     }
                 }
             } else {
@@ -88,8 +111,9 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
                     when (orderStatus.name) {
                         StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_CANCELLED_BY_SELLER),
                         StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_CANCELLED_BY_USER) -> {
+                            binding.textTime.visibility = View.GONE
+                            //binding.textTimeNew.visibility = View.GONE
                             binding.textStatusDesc.visibility = View.GONE
-                            binding.imageStatus.visibility = View.GONE
                             binding.viewCircle.setBackgroundResource(R.drawable.ic_cancel_status)
                             val width = binding.viewCircle.resources.getDimension(R.dimen.status_dot_small_size)
                             val params = binding.viewCircle.layoutParams
@@ -98,10 +122,12 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
                             binding.viewCircle.layoutParams = params
                             binding.textStatus.setTextSize(TypedValue.COMPLEX_UNIT_PX, binding.textStatus.context.resources.getDimension(R.dimen.status_small_size))
                             binding.textStatus.setTextColor(ContextCompat.getColor(binding.textStatus.context, R.color.shimmerColor))
+                            binding.textTime.setTextColor(ContextCompat.getColor(binding.textStatus.context, R.color.shimmerColor))
                         }
                         else -> {
+                            binding.textTime.visibility = View.GONE
+                            //binding.textTimeNew.visibility = View.GONE
                             binding.textStatusDesc.visibility = View.GONE
-                            binding.imageStatus.visibility = View.GONE
                             binding.viewCircle.setBackgroundResource(R.drawable.ic_checked)
                             val width = binding.viewCircle.resources.getDimension(R.dimen.status_dot_small_size)
                             val params = binding.viewCircle.layoutParams
@@ -110,11 +136,13 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
                             binding.viewCircle.layoutParams = params
                             binding.textStatus.setTextSize(TypedValue.COMPLEX_UNIT_PX, binding.textStatus.context.resources.getDimension(R.dimen.status_small_size))
                             binding.textStatus.setTextColor(ContextCompat.getColor(binding.textStatus.context, R.color.shimmerColor))
+                            binding.textTime.setTextColor(ContextCompat.getColor(binding.textStatus.context, R.color.shimmerColor))
                         }
                     }
                 } else {
+                    binding.textTime.visibility = View.GONE
+                    //binding.textTimeNew.visibility = View.GONE
                     binding.textStatusDesc.visibility = View.GONE
-                    binding.imageStatus.visibility = View.GONE
                     binding.viewCircle.setBackgroundResource(R.drawable.bg_rounded_grey_stroke)
                     val width = binding.viewCircle.resources.getDimension(R.dimen.status_dot_small_size)
                     val params = binding.viewCircle.layoutParams
@@ -127,28 +155,6 @@ class OrderTimelineAdapter(private val context: Context, private val orderStatus
             }
             binding.textStatus.text = orderStatus.name
             binding.textStatusDesc.text = StatusHelper.getMessageDetail(orderStatus.name)
-            when(orderStatus.name){
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_PLACED)->{
-                    binding.imageStatus.setImageResource(R.drawable.ic_status_placed)
-                }
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_ACCEPTED)->{
-                    binding.imageStatus.setImageResource(R.drawable.ic_status_cooking)
-                }
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_OUT_FOR_DELIVERY)->{
-                    binding.imageStatus.setImageResource(R.drawable.ic_status_out_for_delivery)
-                }
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_READY)->{
-                    binding.imageStatus.setImageResource(R.drawable.ic_status_ready)
-                }
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_DELIVERED),
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_COMPLETED) ->{
-                    binding.imageStatus.setImageResource(R.drawable.ic_status_completed_delivered)
-                }
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_REFUND_INITIATED),
-                StatusHelper.getStatusMessage(AppConstants.ORDER_STATUS_REFUND_COMPLETED) ->{
-                    binding.imageStatus.setImageResource(R.drawable.ic_status_refund)
-                }
-            }
         }
     }
 
